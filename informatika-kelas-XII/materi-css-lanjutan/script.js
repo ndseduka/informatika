a50fd5f9847211d343e39e3acc6866a2fecfe306
ndseduka <108400344+ndseduka@users.noreply.github.com>
2026-02-08 10:22:56 +0700

    Rename slides to materiSlide and update functions

--- informatika-kelas-XII/materi-css-lanjutan/script.js
+++ informatika-kelas-XII/materi-css-lanjutan/script.js
@@ -1,11 +1,16 @@
-const slides = {
-    pendahuluan: "https://docs.google.com/presentation/d/e/SLIDE_ID_1/embed?start=false&loop=false&delayms=3000",
+const materiSlide = {
+    pendahuluan: "https://docs.google.com/presentation/d/e/SLIDE_ID_1/embed?start=false&loop=false",
     instalasi: "https://docs.google.com/presentation/d/e/SLIDE_ID_2/embed?start=false&loop=false",
     hello: "https://docs.google.com/presentation/d/e/SLIDE_ID_3/embed?start=false&loop=false",
     variabel: "https://docs.google.com/presentation/d/e/SLIDE_ID_4/embed?start=false&loop=false",
     loop: "https://docs.google.com/presentation/d/e/SLIDE_ID_5/embed?start=false&loop=false"
 };
 
-function loadSlide(materi) {
-    document.getElementById("slideFrame").src = slides[materi];
+function loadMateri(materi) {
+    document.getElementById("slideFrame").src = materiSlide[materi];
 }
+
+/* Load materi pertama otomatis */
+window.onload = function () {
+    loadMateri('pendahuluan');
+};
