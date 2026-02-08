6e44a8d84863f65ef496d6bf91d157e10bbce153
ndseduka <108400344+ndseduka@users.noreply.github.com>
2026-02-08 09:59:58 +0700

    Update script.js

--- informatika-kelas-XII/materi-css-lanjutan/script.js
+++ informatika-kelas-XII/materi-css-lanjutan/script.js
@@ -1,8 +1,9 @@
-const slide1 = "https://docs.google.com/presentation/d/e/SLIDE1/pub?start=false&loop=false";
-const slide2 = "https://docs.google.com/presentation/d/e/SLIDE2/pub?start=false&loop=false";
-const slide3 = "https://docs.google.com/presentation/d/e/SLIDE3/pub?start=false&loop=false";
-const slide4 = "https://docs.google.com/presentation/d/e/SLIDE4/pub?start=false&loop=false";
-const slide5 = "https://docs.google.com/presentation/d/e/SLIDE5/pub?start=false&loop=false";
+// GANTI LINK GOOGLE SLIDE DI SINI
+const slide1 = "https://docs.google.com/presentation/d/e/2PACX-1vSLIDE1/pub?start=false&loop=false";
+const slide2 = "https://docs.google.com/presentation/d/e/2PACX-1vSLIDE2/pub?start=false&loop=false";
+const slide3 = "https://docs.google.com/presentation/d/e/2PACX-1vSLIDE3/pub?start=false&loop=false";
+const slide4 = "https://docs.google.com/presentation/d/e/2PACX-1vSLIDE4/pub?start=false&loop=false";
+const slide5 = "https://docs.google.com/presentation/d/e/2PACX-1vSLIDE5/pub?start=false&loop=false";
 
 function loadSlide(url) {
   document.getElementById("slideFrame").src = url;
